b8e28a42e82fd10f2faca4b9f88d667ac5191aa8
RomanAnchugov <romagn@mail.ru>
2017-08-09 13:30:44 +0300

    unit 12 problems

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by romananchugov on 09.08.17.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+}
